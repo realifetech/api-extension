f8c519c7f93668ff813ad82d085905d1f24238f7
Dasha Syvyuk <dashasyvyuk@Dashas-MBP.localdomain>
2021-03-31 18:55:23 +0300

    add new lines

--- src/Exception/EventDispatchException.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace RL\Exception;
-
-class EventDispatchException extends \Exception
-{
-
-}
--- src/Exception/InvalidIRIResourceException.php
+++ src/Exception/InvalidIRIResourceException.php
@@ -4,5 +4,4 @@ namespace RL\Exception;
 
 class InvalidIRIResourceException extends ApiException
 {
-
 }
